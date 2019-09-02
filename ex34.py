7df89ac626554d5e07c376796820b54d7df57e1c
ceciliawambui <47850604+ceciliawambui@users.noreply.github.com>
2019-09-02 17:12:37 +0300

    Add files via upload

---
+++ ex34.py
@@ -0,0 +1,5 @@
+animals = ['bear', 'python 3.6', 'peacock', 'kangaroo', 'whale', 'platypus']
+print("The animal at 1 is", animals[1])
+print("The animal at 3 is", animals[3])
+print("The animal at 2 is", animals[2])
+print("The animal at 4 is", animals[4])
